62d2559a965780eda6b8db04fede84b22c918e58
Catharin <catharin@Cathie.localdomain>
2023-02-25 10:38:09 +0200

    second day with hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { Provider } from "react-redux";
 import { Catalog } from "./components/Catalog/Catalog";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Navigation } from "./components/Navigation/Navigation";
+import { store } from "./store";
 
 export const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <header>
         <Header />
       </header>
@@ -14,6 +16,6 @@ export const App = () => {
         <Catalog />
       </main>
       <Footer />
-    </>
+    </Provider>
   );
 };
