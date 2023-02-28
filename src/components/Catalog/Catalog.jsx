add04bbd6ebfe2f84ef063451524f43e1edc9876
Catharin <catharin@Cathie.localdomain>
2023-02-28 19:44:53 +0200

    3 day with part of hw

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { productRequestAsync } from "../../store/product/productSlice";
 import { CatalogProduct } from "../CatalogProduct/CatalogProduct";
 import { Container } from "../Container/Container";
 import { Order } from "../Order/Order";
@@ -5,28 +8,30 @@ import style from "./Catalog.module.css";
 
 // import "./product.css";
 
-const goodsList = [
-  { title: "Мясная бомба" },
-  { title: "Супер сырный" },
-  { title: "Сытный" },
-  { title: "Итальянский" },
-  { title: "Вечная классика" },
-  { title: "Тяжелый удар" },
-];
-
 export const Catalog = () => {
+  const { productsList } = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+  const { category, activeCategory } = useSelector((state) => state.category);
+
+  useEffect(() => {
+    console.log("Kat");
+    if (category.length) {
+      dispatch(productRequestAsync(category[activeCategory].title));
+    }
+  }, [category, activeCategory]);
+
   return (
     <section className={style.catalog}>
       <Container className={style.container}>
         <Order />
         <div className={style.wrapper}>
-          <h2 className={style.title}>Бургеры</h2>
+          <h2 className={style.title}>{category[activeCategory]?.rus}</h2>
 
           <div className={style.wrap_list}>
             <ul className={style.list}>
-              {goodsList.map((item, i) => (
-                <li key={i} className={style.item}>
-                  <CatalogProduct title={item.title} />
+              {productsList.map((item) => (
+                <li key={item.id} className={style.item}>
+                  <CatalogProduct item={item} />
                 </li>
               ))}
             </ul>
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,25 +1,34 @@
 import classNames from "classnames";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { changeCategory } from "../../store/category/categorySlice";
+import {
+  categoryRequestAsync,
+  changeCategory,
+} from "../../store/category/categorySlice";
 import { Container } from "../Container/Container";
 import style from "./Navigation.module.css";
+import { API_URI, POSTFIX } from "../../const";
 
 export const Navigation = () => {
   const { category, activeCategory } = useSelector((state) => state.category);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(categoryRequestAsync());
+  }, []);
 
   return (
     <nav className={style.navigation}>
       <Container className={style.container}>
         <ul className={style.list}>
           {category.map((item, i) => (
-            <li key={i} className={style.item}>
+            <li key={item.title} className={style.item}>
               <button
                 className={classNames(
                   style.button,
                   activeCategory === i ? style.button_active : ""
                 )}
-                style={{ backgroundImage: `url(${item.image})` }}
+                style={{ backgroundImage: `url(${API_URI}/${item.image})` }}
                 onClick={() => {
                   dispatch(changeCategory({ indexCategory: i }));
                 }}
